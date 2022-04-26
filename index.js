df63bcee87957aa938ad362e471e3842f90ea51b
+ <diazcristina.la@gmail.com>
2022-04-26 09:43:09 -0700

    commit tree 6b5477841cc96e00fb1d17de30b93a3c62692e3c

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = function () {
+  return 2000 / 100;
+};
+divide();
+
+const square = (parameter) => parameter ** 2;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
